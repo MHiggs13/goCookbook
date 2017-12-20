acc6bbe107b9f56996891f73701b662473f1c0be
MHiggs13 <higgs1101@gmail.com>
2017-12-20 20:07:19 +0000

    completed equivalent binary tree exercises

--- exercises/exeEquivBinaryTrees.go
+++ exercises/exeEquivBinaryTrees.go
@@ -2,16 +2,56 @@ package main
 
 import (
   "golang.org/x/tour/tree"
+  "fmt"
 )
+
+func Walk(t *tree.Tree, ch chan int) {
+  _walk(t, ch)
+  close(ch)
+}
 
 // Walk walks the tree t sending all values
 // from the tree to channel ch.
-func Walk(t *tree.Tree, ch chan int)
+func _walk(t *tree.Tree, ch chan int) {
+  if t != nil {
+    _walk(t.Left, ch)
+    ch <- t.Value
+    _walk(t.Right, ch)
+  }
+}
 
 // Same determines whether the trees
 // t1 and t2 contain the same values.
-func Same(t1, t2 *tree.Tree) bool
+func Same(t1, t2 *tree.Tree) bool {
+  ch1 := make(chan int)
+  ch2 := make(chan int)
+  go Walk(t1, ch1)
+  go Walk(t2, ch2)
+
+  for v := range ch1 {
+    if v != <-ch2 {
+      return false
+    }
+  }
+  return true
+
+
+}
 
 func main() {
+  // Implement Walk
+  // Test Walk
+  // Implement Same using Walk to determine whether t1 and t2 store the same values
+  // Test Same
+
+  ch := make(chan int)
+  go Walk(tree.New(1), ch)
+  for v := range ch {
+    fmt.Println(v)
+  }
+
+  fmt.Println(Same(tree.New(1), tree.New(2)))
+  fmt.Println(Same(tree.New(1), tree.New(1)))
+  fmt.Println(Same(tree.New(2), tree.New(2)))
 
 }
